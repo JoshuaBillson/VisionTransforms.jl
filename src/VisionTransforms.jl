559e9da402ed9cce8e4425749f1173d78db6a592
JoshuaBillson <jmbillson@outlook.com>
2024-09-20 13:54:32 -0600

    Implemented Image2D and Mask2D

--- src/VisionTransforms.jl
+++ src/VisionTransforms.jl
@@ -10,6 +10,7 @@ include("methods.jl")
 export image2tensor, tensor2image, imresize, crop, flipX, flipY, rot90
 
 include("transforms.jl")
+export Image2D, Mask2D
 export Mask, Image, NoOp, AbstractTransform
 export apply_transform, apply
 export Resize, RandomCrop, FlipX, FlipY, Rot90, ComposedTransform
